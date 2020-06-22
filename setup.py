76cf801e12c0cbe6923ffbd5d7bbcf34b9392087
Mathieu Guilbault <mguilbault@gmail.com>
2020-06-21 21:34:49 -0400

    Refactor the files

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
-version="0.0.1",
+from setuptools import setup
+
+setup(
+    name="test_package",
+    packages=["test_package"],
+    version="0.0.1",
+    description="This is a test for package creation",
+    author="Mathieu Guilbault",
+    url="https://github.com/mathonweb/test_package",
+    download_url="https://github.com/mathonweb/test_package/releases",
+    install_requires=[],
+)
